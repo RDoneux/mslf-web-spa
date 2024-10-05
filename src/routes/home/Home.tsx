697d4000ff4f7ca94649a41292317f0f41a0e722
Rob Doneux <robertdoneux@gmail.com>
2024-10-05 20:17:15 +0100

    updates project target for mslf development

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -3,7 +3,7 @@ import styles from './Home.module.css';
 export default function Home() {
   return (
     <div className={styles['container']}>
-      <h1>TPLT | WEB SPA</h1>
+      <h1>MSLF | WEB SPA</h1>
     </div>
   );
 }
