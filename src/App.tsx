2ce90c8e0e813d6db739efdfed81b63be8c544f4
Rob Doneux <robertdoneux@gmail.com>
2024-10-05 20:59:21 +0100

    feat/mslf-nav-bar: adds navigation bar with 3 links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './routes/home/Home';
 import NotFound from './routes/not-found/NotFound';
+import NavBar from './components/nav-bar/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route index element={<Home />} />
         <Route path="*" element={<NotFound />} />
---
+++ src/components/nav-bar/NavBar.tsx
@@ -0,0 +1,41 @@
+import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.css';
+
+export default function NavBar() {
+  return (
+    <nav className={styles['navigation']}>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? styles['active-link'] : ''
+            }
+          >
+            HOME
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/poems"
+            className={({ isActive }) =>
+              isActive ? styles['active-link'] : ''
+            }
+          >
+            POEMS
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/blog"
+            className={({ isActive }) =>
+              isActive ? styles['active-link'] : ''
+            }
+          >
+            BLOG
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
