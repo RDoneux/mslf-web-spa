b9885be399df8ac1d4d3c2515443e92408011d44
Rob Doneux <robertdoneux@gmail.com>
2024-10-06 08:36:35 +0100

    feat/home-page: adds homepage components: - temp profile image - temp author name - enter button -> poem page

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,9 +1,23 @@
+import profileImage from '/profile-image.jpg';
+
 import styles from './Home.module.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Home() {
   return (
     <div className={styles['container']}>
-      <h1>MSLF | WEB SPA</h1>
+      <h1>James Smith</h1>
+
+      <div className={styles['profile-image-wrapper']}>
+        <img src={profileImage} alt="Author profile image" />
+      </div>
+
+      <div className={styles['enter-wrapper']}>
+        <h2>Poet & Blogger</h2>
+        <NavLink to="poems">
+          <button>ENTER</button>
+        </NavLink>
+      </div>
     </div>
   );
 }
