eb99984258e0b92cce037b494707cf96b43809cc
Rob Doneux <robertdoneux@gmail.com>
2024-10-06 07:44:28 +0100

    feat/footer: adds mobile breakpoints for footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,6 @@ import { version } from '../../../package.json';
 export default function Footer() {
   return (
     <footer className={styles['footer']}>
-      <span></span>
       <span className={styles['app-title']}>MYSELF</span>
       <span className={styles['app-version']}>App Version: {version}</span>
     </footer>
