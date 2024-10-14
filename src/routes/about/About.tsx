ff952ed18e17bf5801face5e2afea089ef6b4a38
Rob Doneux <robertdoneux@gmail.com>
2024-10-14 16:41:53 +0100

    fix: adds quill to about page

--- src/routes/about/About.tsx
+++ src/routes/about/About.tsx
@@ -3,6 +3,7 @@ import styles from './About.module.css';
 import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 import IAbout from './interfaces/IAbout';
+import ReactQuill from 'react-quill';
 
 export default function About() {
   const [aboutInfo, setAboutInfo] = useState<IAbout>();
@@ -18,7 +19,7 @@ export default function About() {
   return (
     <div className={styles['container']}>
       <h1>ABOUT</h1>
-      <p>{aboutInfo?.inspiration}</p>
+      <ReactQuill theme="bubble" readOnly={true} value={aboutInfo?.inspiration} />
     </div>
   );
 }
