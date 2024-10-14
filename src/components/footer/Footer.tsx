32e8eb440fad647f83488cf3dd2d29c3a39040b0
Rob Doneux <robertdoneux@gmail.com>
2024-10-14 16:46:14 +0100

    fix: updates project name

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,10 @@
 import styles from './Footer.module.css';
-import { version } from '../../../package.json';
+import { version, name } from '../../../package.json';
 
 export default function Footer() {
   return (
     <footer className={styles['footer']}>
-      <span className={`${styles['app-title']} dots`}>MYSELF</span>
+      <span className={`${styles['app-title']} dots`}>{name}</span>
       <span className={styles['app-version']}>App Version: {version}</span>
     </footer>
   );
